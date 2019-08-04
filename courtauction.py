613a491d3195cc8e534cda53b295aa3aa698fe5b
Juhwan Yoo <electopx@gmail.com>
2019-08-05 05:28:13 +0900

    Update courtauction.py

--- courtauction.py
+++ courtauction.py
@@ -6,6 +6,11 @@ import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+import sys
+import datetime
+import mysql.connector
+from sqlalchemy import create_engine
+from mysql.connector import errorcode
 
 #driver = webdriver.Chrome('../driver/chromedriver')
 #driver = webdriver.PhantomJS('../driver/phantomjs')
@@ -23,12 +28,12 @@ driver.find_element_by_xpath('//*[@id="rltyct2"]').click()  # 주택
 driver.find_element_by_xpath('//*[@id="rltyct7"]').click()  # 오피스텔
 driver.find_element_by_xpath('//*[@id="kyg_srch_option"]/table/tbody/tr[9]/td/input[1]').click()    # 검색하기
 
-# 텍스트 보기
+# 100개씩
 driver.implicitly_wait(3)
 driver.find_element_by_xpath('//*[@id="list_limit"]').click()
 driver.find_element_by_xpath('//*[@id="list_limit_combo"]/li[6]/nobr').click()
 
-# 100 개씩 보기
+# 텍스트 보기
 driver.implicitly_wait(3)
 driver.find_element_by_xpath('//*[@id="kyg_list"]/table[1]/tbody/tr/td[8]').click()
 
@@ -44,7 +49,7 @@ soup = BeautifulSoup(html, 'html.parser')
 #status_list = [raw_list[n].find('span').get_text() for n in range(0, len(raw_list))]
 #print (status_list)
 
-pd_data = pd.DataFrame(columns=['사건번호', '소재지', '용도','감정가 최저가', '상태', '매각기일'])
+pd_data = pd.DataFrame(columns=['사건번호', '소재지', '용도','감정가/최저가', '상태', '매각기일'])
 re_td = re.compile(r'(\<td[^>]+[\>])([^<]*)(\<\/td\>)')
 
 table = soup.find('table', { 'id': 'kyg_list_table' })      # <table id="kyg_list_table">을 찾음
@@ -70,5 +75,56 @@ for tr in table.find_all('tr'):                             # 모든 <tr> 태그
         if td_data is not '': data.append(td_data)          # data 리스트에 td 데이터 저장
     if len(data) is 6: pd_data.loc[len(pd_data)] = data
 print (pd_data)
-
+#print (pd_data['감정가/최저가'])
 driver.close()
+
+user = 'user'
+password = ''
+host = ''
+database = ''
+config = {}
+
+args = sys.argv[0:]
+optionLen = len(args)
+
+def init():
+
+    global user, password, host, database, config
+
+    if (len(args) <= 1):
+        print('[ERR] There is no option')
+        return False
+
+    for i in range(optionLen-1):
+        data = str(args[i+1])
+        if args[i].upper() == '-U':		# -U : user name of MySQL (e.g.: root)
+            user = data
+        elif args[i].upper() == '-P':	# -P : password of username
+            password = data
+        elif args[i].upper() == '-H':	# -H : host of MySQL (e.g.: 127.0.0.1)
+            host = data
+        elif args[i].upper() == '-D':	# -D : database name (e.g.: wp_aitest)
+            database = data
+
+    if (user == '') or (password == '') or (host == '') or (database == ''):
+        print('[ERR] Please input all required data like user, password, host and database name.')
+        return False
+
+    return True
+
+if init():
+    try:
+        engine = create_engine('mysql+pymysql://'+user+':'+password+'@'+host+':3306/'+database, encoding='utf-8')
+        pd_data.to_sql(name='interauction', con=engine, if_exists = 'replace')
+        print('[OK] Connection success')
+
+    # Exception for connection
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('[ERR] Something is wrong with your user name or password')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print('[ERR] Database does not exist')
+        else:
+            print('[ERR]', err)
+else:
+    print("[ERR] Connection failure")
